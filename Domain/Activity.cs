55f3cdf2b49f5e9791b48085c2f47c06e1085720
alyonazakharova <alenazakharovaspb@yandex.ru>
2023-02-13 00:01:41 +0700

    some ui logic + minor fixes

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Domain
 {
-    public class Activity
+  public class Activity
     {
         public Guid Id { get; set; }
         public string Title { get; set; }

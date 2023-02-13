2eda34486ce5f483811491282ec16fbf16fca3a8
alyonazakharova <alenazakharovaspb@yandex.ru>
2023-02-13 17:05:02 +0700

    crud functionality

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Button, Card, Icon, Image } from 'semantic-ui-react'
+import { Button, Card, Image } from 'semantic-ui-react'
 import { Activity } from '../../../app/models/activity'
 
 interface Props {
   activity: Activity;
   cancelSelectActivity: () => void;
+  openForm: (id: string) => void;
 }
 
-export default function ActivityDetails({activity, cancelSelectActivity}: Props) {
+export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props) {
   return (
     <Card>
       <Image src={`/assets/categoryImages/${activity.category}.jpg`}/>
@@ -21,7 +21,7 @@ export default function ActivityDetails({activity, cancelSelectActivity}: Props)
       </Card.Content>
       <Card.Content extra>
         <Button.Group widths='2'>
-          <Button color='blue' content='Edit'/>
+          <Button onClick={() => openForm(activity.id)} color='blue' content='Edit'/>
           <Button onClick={cancelSelectActivity} color='grey' content='Cancel'/>
         </Button.Group>
       </Card.Content>
